02791e0296c05f8f4959d6682e3506a28cf31b33
Michael Probst <mpprobst@gmail.com>
2021-01-25 10:00:28 -0500

    Make Prefabs

---
+++ Scavenger/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public abstract class MovingObject : MonoBehaviour
+{
+    public float moveTime = 0.1f;            
+    public LayerMask blockingLayer;           
+
+
+    private BoxCollider2D boxCollider;         
+    private Rigidbody2D rb2D;               
+    private float inverseMoveTime;         
+
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        rb2D = GetComponent<Rigidbody2D>();
+
+        //By storing the reciprocal of the move time we can use it by multiplying instead of dividing, this is more efficient.
+        inverseMoveTime = 1f / moveTime;
+    }
+
+
+    //Move returns true if it is able to move and false if not. 
+    //Move takes parameters for x direction, y direction and a RaycastHit2D to check collision.
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+
+        Vector2 end = start + new Vector2(xDir, yDir);
+
+        boxCollider.enabled = false;
+
+        //Cast a line from start point to end point checking collision on blockingLayer.
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+
+        boxCollider.enabled = true;
+
+        //Check if anything was hit
+        if (hit.transform == null)
+        {
+            //If nothing was hit, start SmoothMovement co-routine passing in the Vector2 end as destination
+            StartCoroutine(SmoothMovement(end));
+
+            //Return true to say that Move was successful
+            return true;
+        }
+
+        //If something was hit, return false, Move was unsuccesful.
+        return false;
+    }
+
+
+    //Co-routine for moving units from one space to next, takes a parameter end to specify where to move to.
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        //Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+        //Square magnitude is used instead of magnitude because it's computationally cheaper.
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        //While that distance is greater than a very small amount (Epsilon, almost zero):
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            //Find a new position proportionally closer to the end, based on the moveTime
+            Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPostion);
+
+            //Recalculate the remaining distance after moving.
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+            yield return null;
+        }
+    }
+
+
+    //AttemptMove takes a generic parameter T to specify the type of component we expect our unit to interact with if blocked (Player for Enemies, Wall for Player).
+    protected virtual void AttemptMove<T>(int xDir, int yDir)
+        where T : Component
+    {
+        RaycastHit2D hit;
+
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            //If nothing was hit, return and don't execute further code.
+            return;
+
+        //Get a component reference to the component of type T attached to the object that was hit
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        //If canMove is false and hitComponent is not equal to null, meaning MovingObject is blocked and has hit something it can interact with.
+        if (!canMove && hitComponent != null)
+
+            //Call the OnCantMove function and pass it hitComponent as a parameter.
+            OnCantMove(hitComponent);
+    }
+
+
+    //The abstract modifier indicates that the thing being modified has a missing or incomplete implementation.
+    //OnCantMove will be overriden by functions in the inheriting classes.
+    protected abstract void OnCantMove<T>(T component)
+        where T : Component;
+}
